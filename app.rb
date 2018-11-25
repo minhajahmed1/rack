f5d34d2828c6f2badd1f3c27716cd0ee66fd00d5
Minhaj Ahmed <minhajahmed32@hotmail.com>
2018-11-24 21:16:29 -0800

    last

--- app.rb
+++ app.rb
@@ -7,35 +7,39 @@ require_relative 'admin'
 
 
 class App
-
+  # method recieves an arguement from rack and returns http response object
   def call(env)
     response_headers = {}
     request_cookies = Rack::Utils.parse_cookies(env)
     route = Route.new(env)
     status = route.name =~ /^\d\d\d$/ ? route.name.to_i : 200
-    template_data = {
-    	visit_count: request_cookies["session_count"].to_i+1
-    }
-    if env["REQUEST_METHOD"] == "POST"
-    	params = CGI::parse(env["rack.input"].read)
-    	template_data[:employability] = params["employability"] && params ["employability"].first
-    	template_data[:nationality] = params["nationality"] && params["nationality"].first
-      template_data[:income] = params["income"] && params["income"].first
-      template_data[:email] = params["email"] && params["email"].first
-    end
+    template_data = {}
     template = Template.new(route.name, template_data)
  
    	if env["REQUEST_METHOD"] == "POST"
-  		  json = template_data.to_json
-  		  open('answers.json', 'a') do |file|
-  	    	file.puts json 
+      params = CGI::parse(env["rack.input"].read)
+      template_data[:employability] = params["employability"] && params ["employability"].first
+      template_data[:nationality] = params["nationality"] && params["nationality"].first
+      template_data[:income] = params["income"] && params["income"].first
+      template_data[:email] = params["email"] && params["email"].first
+        if new_survey?(template_data[:email])
+    		  open('answers.json', 'w') do |file|
+            answers = existing_answers << template_data
+          file.write answers 
+          end
         end
     end
  
     [status, response_headers, [template.render]]
 
   end
+  def existing_answers
+    JSON.parse(File.read "answers.json") rescue []
+  end
 
+  def new_survey? email
+    !existing_answers.find {|a| a[:email] == email}
+  end
 end  		
 
 
